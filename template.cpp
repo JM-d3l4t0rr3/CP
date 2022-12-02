154f8ef489f05668f9a3a7354c2a1a3749e9ff87
JM-d3l4t0rr3 <72047760+JM-d3l4t0rr3@users.noreply.github.com>
2022-12-01 20:23:56 -0500

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,4 +6,22 @@ using namespace std;
 int main(){
     int n; cin >> n;
     cout << "hola mundo" <<endl;
+    n = stoi("1234");// cast string to integer
+    
+    
+    vector<int> v; // Define vectors
+  
+    sort(v.begin(), v.end()); //Ascending
+    sort(v.begin(), v.end(),  greater<int>()); // Descending order
+     
+    for(int a: v){
+        cout<< a << endl; //loop through an array / vector
+    }
+
+    fstream MyReadFile("input.txt");
+    while (getline (MyReadFile, myText)) {
+        // Output the text from the file
+        cout << myText << endl;
+    }
+    MyReadFile.close();
 }
